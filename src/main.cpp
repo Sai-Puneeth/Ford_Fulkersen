39cd5aaf88273cb5f909a1727c4a8de0816ef13d
Sai-Puneeth <spuneeth987@gmail.com>
2020-04-14 19:24:53 +0530

    main

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include <chrono> 
+#include "Ford_Fulkersen.h"
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+    std::ifstream in("../data/1.txt");
+    std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
+    std::cin.rdbuf(in.rdbuf());
+    auto start = high_resolution_clock::now();
+
+
+
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << duration.count() << endl;
+    return 0;
+}
